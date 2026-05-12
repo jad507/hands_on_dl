81aa5e859fa26c9bdb15b3fa8b81d22e27b8dc83
jad507 <jad507@psu.edu>
2026-05-12 15:33:59 -0400

    ran align.py and group_speakers.py. Run took about 20 seconds. Minor edit to run_overnight so that it can handle last of the log output that was previous lost in the buffer

--- run_overnight.ps1
+++ run_overnight.ps1
@@ -1,3 +1,4 @@
+$env:PYTHONUNBUFFERED = "1"
 python audio_pipeline/diarize.py 2>&1 | tee logs/diarize.log
 if ($LASTEXITCODE -eq 0) {
     python audio_pipeline/transcribe.py 2>&1 | tee logs/transcribe.log
